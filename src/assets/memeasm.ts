469721672d5e514daea2de277fcdefddbb74c200
xarantolus <xarantolus@protonmail.com>
2022-10-05 23:36:49 +0200

    Improve editor

---
+++ src/assets/memeasm.ts
@@ -0,0 +1,40 @@
+import * as monaco from 'monaco-editor/esm/vs/editor/editor.api'
+
+let configuration: monaco.languages.LanguageConfiguration = {
+	"comments": {
+		lineComment: "//",
+	},
+	// symbols used as brackets
+	"brackets": [],
+	// symbols that are auto closed when typing
+	"autoClosingPairs": [],
+	// symbols that can be used to surround a selection
+	"surroundingPairs": [],
+	// If we don't add a pattern that includes "//", the comment suggestion snippet won't work
+	"wordPattern": new RegExp("(\\b(\\S|\\s){2,}\\b|\\S)")
+}
+
+import snippets_raw from './snippets.json';
+
+function createSnippets(range: monaco.IRange) {
+	let snippets: monaco.languages.CompletionItem[] = [];
+	for (let key of Object.keys(snippets_raw)) {
+		let snippet = (snippets_raw as any)[key];
+		snippets.push({
+			label: key,
+			kind: monaco.languages.CompletionItemKind.Snippet,
+			insertText: snippet.body.join('\n'),
+			documentation: snippet.description,
+			range: range
+		});
+	}
+	console.log(snippets);
+
+	return snippets;
+}
+
+
+export {
+	configuration,
+	createSnippets
+}
--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,48 +1,77 @@
 <script lang="ts" setup>
-import * as monaco from 'monaco-editor/esm/vs/editor/editor.api'
+import * as monaco from 'monaco-editor'
 import { ref, onMounted } from 'vue'
 
-import customLangMonarch from '../assets/custom-lang';
+import { configuration, createSnippets } from '../assets/memeasm';
 
-monaco.languages.register({ id: 'custom' })
-monaco.languages.setMonarchTokensProvider('custom', customLangMonarch as monaco.languages.IMonarchLanguage)
+monaco.languages.register({ id: 'memeasm', extensions: ['.memeasm'], aliases: ['memeasm', 'Memeasm', "MemeAssembly"] });
+monaco.languages.setLanguageConfiguration('memeasm', configuration)
+
+// monaco.languages.registerCompletionItemProvider('memeasm', {
+// 	provideCompletionItems(model, position, context, token) {
+// 		console.log("Providing completion");
+// 		let range = new monaco.Range(position.lineNumber, position.column, position.lineNumber, position.column+1)
+// 		return {
+// 			suggestions: createSnippets(range)
+// 		};
+// 	},
+// })
 
 const editor = ref()
 
 onMounted(() => {
 	let monaco_editor = monaco.editor.create(editor.value, {
-		value: `// Type source code in your language here...
-class MyClass {
-  @attribute
-  void main() {
-    Console.writeln("Hello Monarch World!\\n");
-  }
-}`,
-		language: 'custom',
+		value: `I like to have fun, fun, fun, fun, fun, fun, fun, fun, fun, fun main
+	rax is brilliant, but I like a
+
+	What the hell happened here? Print each character
+	upgrade
+
+		who would win? rax or z
+
+		What the hell happened here? rax is equal to z
+
+		z wins
+		What the hell happened here? rax < z
+
+		what can I say except al
+		upvote rax
+
+	fuck go back
+
+	rax wins
+	What the hell happened here? rax > z
+
+	what can I say except \\n
+
+	I see this as an absolute win
+`,
+		language: 'memeasm',
 		theme: 'vs-dark',
 		automaticLayout: true,
+		contextmenu: true,
 	});
 
-	// monaco_editor.addOverlayWidget({
-	// 	getId: () => 'my-widget',
-	// 	getDomNode: () => {
-	// 		const div = document.createElement('div')
-	// 		div.innerHTML = 'Hello world'
-	// 		return div
-	// 	},
-	// 	getPosition: () => null,
-	// });
+	let runMemeAsmFile = () => {
+		alert("Not implemented yet");
+	}
 
-	monaco_editor.addAction({
-		id: 'run-file',
-		label: 'Run MemeAssembly file',
-		keybindings: [
-			monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyM,
-			monaco.KeyMod.CtrlCmd | monaco.KeyCode.Enter,
-		],
-		contextMenuGroupId: 'navigation',
-		contextMenuOrder: 1.5,
-		run: () => alert('My action was triggered!')
+	// Add bottom navigation bar as widget
+	monaco_editor.addOverlayWidget({
+		getId() { return "bottom-navigation" },
+		getDomNode() {
+			// Create a run button
+			let node = document.createElement("button");
+			node.innerText = "Run"
+			node.className = "run-button";
+			node.onclick = runMemeAsmFile;
+			return node;
+		},
+		getPosition() {
+			return {
+				preference: monaco.editor.OverlayWidgetPositionPreference.TOP_RIGHT_CORNER
+			}
+		}
 	});
 })
 </script>
@@ -52,9 +81,24 @@ class MyClass {
 </template>
 
 <style scoped>
+.wrap,
 #editor {
 	text-align: left;
 	width: 100%;
 	height: 100%;
+}
+</style>
+
+<style>
+.run-button {
+	margin: 1em;
+	padding: 0.5em 1em;
+	border: none;
+	border-radius: 0.5em;
+	background-color: #007acc;
+	color: white;
+	font-size: 1em;
+	font-weight: bold;
+	cursor: pointer;
 }
 </style>
