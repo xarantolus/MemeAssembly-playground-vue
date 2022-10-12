12d3603a838f1ab74778e36b6935db1f27051341
xarantolus <xarantolus@protonmail.com>
2022-10-12 16:36:49 +0200

    Fix vite build

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,15 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    target: 'es2020',
+  },
   optimizeDeps: {
     include: [
       `monaco-editor/esm/vs/editor/editor.worker`,
-    ]
+    ],
+    esbuildOptions: {
+      target: 'es2020',
+    },
   }
 })
