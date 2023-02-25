9d18b8beca330188614ede24bc7febae0f5ff755
xarantolus <xarantolus@protonmail.com>
2023-02-25 09:23:14 +0100

    Upgrade dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import wasm from 'vite-plugin-wasm';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [wasm(), vue()],
   build: {
     target: 'es2020',
   },
   optimizeDeps: {
+    exclude: ['ax-x86', 'assembly-script'],
     include: [
       `monaco-editor/esm/vs/editor/editor.worker`,
     ],
